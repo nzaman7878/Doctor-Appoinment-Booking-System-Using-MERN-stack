177d47dd63b5aaee8ef23712eb1216fc7f06d8ef
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-09-15 02:23:53 +0530

    Add SpecialityMenu component for browsing doctors by speciality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import SpcialityMenu from '../components/SpcialityMenu'
 
 const Home = () => {
   return (
     <div>
         <Header />
+        <SpcialityMenu />
     </div>
   )
 }
