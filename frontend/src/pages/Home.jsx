b915be512cda7c2ddf70ba70f94bf211ed9a8181
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-09-18 01:56:29 +0530

    feat: add responsive Banner component with call-to-action button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import SpcialityMenu from '../components/SpcialityMenu'
 import TopDoctors from '../components/TopDoctors'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Header />
         <SpcialityMenu />
         <TopDoctors />
+        <Banner />
     </div>
   )
 }
