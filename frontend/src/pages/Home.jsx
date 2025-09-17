dcffea449868c48937a07dfd2eee678132f64040
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-09-17 23:37:49 +0530

    feat: add appointment page navigation and improve TopDoctors UI with image background & more button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpcialityMenu from '../components/SpcialityMenu'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
     <div>
         <Header />
         <SpcialityMenu />
+        <TopDoctors />
     </div>
   )
 }
