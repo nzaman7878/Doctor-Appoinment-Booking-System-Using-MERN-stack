e461cca8e92a052e9ee31e2877fb93f269abe825
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-10-02 00:35:51 +0530

    feat : added related doctors ui

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets_frontend/assets";
+import RelatedDoctors from "../components/RelatedDoctors";
 
 const Appointment = () => {
   const { docId } = useParams();
@@ -154,6 +155,8 @@ const Appointment = () => {
             Book an appointment
           </button>
         </div>
+        {/* --------- Related Doctors --------- */}
+        <RelatedDoctors docId={docId}  speciality={docInfo.speciality}/>
       </div>
     )
   );
