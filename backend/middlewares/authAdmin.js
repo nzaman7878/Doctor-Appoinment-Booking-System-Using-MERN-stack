da93e3d237943382ee893d9b3b584f9e8637f075
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-11-03 03:04:53 +0530

    added authAdmin middlewares

---
+++ backend/middlewares/authAdmin.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+
+//admin authentication middlewares
+const authAdmin = async (req,res,next) =>{
+
+    const {atoken} = req.headers
+
+    if(!atoken) {
+        return res.json({success:false,message:'Not Authorized Login Again'})
+    }
+
+    const token_decode = jwt.verify(atoken,process.env.JWT_SECRET)
+
+    if (token_decode !== process.env.ADMIN_EMAIL +  process.env.ADMIN_PASSWORD){
+        return res.json({success:false,message:'Not Authorized Login Again'})
+    }
+
+    next()
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export default authAdmin;
