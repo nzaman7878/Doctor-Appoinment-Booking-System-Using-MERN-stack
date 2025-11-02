8ed72e281ab062b97c07a4b3e078af62a2bf5869
nzaman7878 <nuruzzaman31032001@gmail.com>
2025-11-03 02:28:51 +0530

    added admin login api

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import Doctor from "../models/doctorModel.js";
 import { v2 as cloudinary } from "cloudinary";
+import jwt from "jsonwebtoken"
 
 // API for adding doctor
 const addDoctor = async (req, res) => {
@@ -65,4 +66,29 @@ const addDoctor = async (req, res) => {
   }
 };
 
-export { addDoctor };
+// API for admin login
+
+const loginAdmin = async (req, res)=> {
+
+  try {
+    
+    const {email, password} = req.body
+
+    if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+      const token = jwt.sign(email+password,process.env.JWT_SECRET)
+      res.json({success:true,token})
+
+    }
+    else {
+      res.json({success:false,message:"invalid credentials"})
+    }
+
+  } catch (error) {
+    console.log(error)
+    res.json({success:false,message:error.message})
+    
+  }
+}
+
+
+export { addDoctor, loginAdmin };
